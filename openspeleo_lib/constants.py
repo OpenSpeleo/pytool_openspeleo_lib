fa5f78b8d02c74a670beb24cba864a3c425499c9
Jonathan Dekhtiar <jonathan@dekhtiar.com>
2025-11-03 22:13:09 -0500

    [v0.0.7] Constant Updates

--- openspeleo_lib/constants.py
+++ openspeleo_lib/constants.py
@@ -1,12 +1,11 @@
 from __future__ import annotations
 
 # Length for the shot field `name` used by compass
-OSPL_SHOTNAME_MAX_LENGTH = 20
+OSPL_SHOTNAME_MAX_LENGTH = 50
 OSPL_SHOTNAME_DEFAULT_LENGTH = 6
 
 # Length for the section field `name`
-OSPL_SECTIONNAME_MAX_LENGTH = 150
-
+OSPL_SECTIONNAME_MAX_LENGTH = 500
 
 # Maximum retry attempts for any while loop
 OSPL_MAX_RETRY_ATTEMPTS = 100
