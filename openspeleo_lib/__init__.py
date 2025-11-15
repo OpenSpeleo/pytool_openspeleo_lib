eff64923ca0e0ccce231467efe9dfbe6f83e2118
Jonathan Dekhtiar <jonathan@dekhtiar.com>
2025-11-15 07:30:13 -0500

    v 0.0.8 - Validation Fixes

--- openspeleo_lib/__init__.py
+++ openspeleo_lib/__init__.py
@@ -4,7 +4,7 @@
 "A library to read/write and convert OpenSpeleo files"
 """
 
-__version__ = "0.0.7"
+__version__ = "0.0.8"
 
 # Initialize the logger
 from openspeleo_lib import logger  # noqa: F401 # pyright: ignore[reportUnusedImport]
