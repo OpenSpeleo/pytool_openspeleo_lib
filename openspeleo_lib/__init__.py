ec00c798d19a11b792d548bf4832e7bc3db946da
DEKHTIARJonathan <jonathan@dekhtiar.com>
2025-12-29 22:36:34 -0500

    v0.0.9 - Ariane v26 Support

--- openspeleo_lib/__init__.py
+++ openspeleo_lib/__init__.py
@@ -4,7 +4,7 @@
 "A library to read/write and convert OpenSpeleo files"
 """
 
-__version__ = "0.0.8"
+__version__ = "0.0.9"
 
 # Initialize the logger
 from openspeleo_lib import logger  # noqa: F401 # pyright: ignore[reportUnusedImport]
